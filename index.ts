885d932c827a95c7996fbdbf6553883a29a49a7e
fatima <162475613+fatimaRiaz531@users.noreply.github.com>
2024-05-19 08:02:05 +0500

    CountDown Timer project 9

---
+++ index.ts
@@ -0,0 +1,40 @@
+import { differenceInSeconds } from "date-fns";
+import inquirer from "inquirer";
+
+const res =await inquirer.prompt({
+    type: "number",
+    name: "UserInput",
+    message: "Enter amount of Second",
+    validate: (Input) =>{
+        if (isNaN(Input)){
+            return "Please enter valid number"
+
+        } else if (Input>60){
+            return "Second must within 60"
+
+        } else {
+            return true;
+        }
+    }
+
+});
+
+let input =res.UserInput
+
+function startTime(val: number){
+    const intTime = new Date().setSeconds(new Date().getSeconds() + val)
+    const intervalTime = new Date(intTime);
+    setInterval(()=>{
+        const currentTime = new Date()
+        const timeDiff = differenceInSeconds(intervalTime, currentTime)
+        if(timeDiff <=0){
+            console.log('Time has expired')
+            process.exit();
+        }
+        const min = Math.floor((timeDiff % (3600 * 24)) / 3600) // show minutes
+        const sec = Math.floor(timeDiff% 60);
+        console.log(`${min.toString().padStart(2, "0")}:${sec.toString().padStart(2,"0")}`)
+    }, 1000);
+}
+
+startTime(input);
